f0150dd2a55e44a62822c146d724d4745c997481
ddeveloper72 <duncan.niamh@gmail.com>
2018-11-24 18:00:00 +0000

    Created the login.html and setup login view and login url.

--- accounts/views.py
+++ accounts/views.py
@@ -14,3 +14,7 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have been successfully logged out!")
     return redirect(reverse('index'))
+
+def login(request):
+    """Return a login page"""
+    return render(request, 'login.html')
