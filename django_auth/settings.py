6c6e34464b86741a7e1df17323da55fa2341146f
ddeveloper72 <duncan.niamh@gmail.com>
2018-11-27 21:10:58 +0000

    Added a location for static files, configured the settings and added a local css style sheet.

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static") # setup the location of the static files (javascript, css, images)
+]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
